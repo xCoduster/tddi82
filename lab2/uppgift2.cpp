a9782e005a601e51514c55ef7aaa8cc26a4ed2b8
matol699 <matol699@student.liu.se>
2025-05-07 17:11:55 +0200

    use std::string instead of int

--- lab2/uppgift2.cpp
+++ lab2/uppgift2.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <string>
 
 #include "counted_ptr.h"
 #include "node.h"
 
-//Komplettering: Ändra så att uppgift2 använder sig av std::string enligt uppgiftsbeskrivningen.
+// Komplettering: Ändra så att uppgift2 använder sig av std::string enligt uppgiftsbeskrivningen.
 
-counted_ptr<Node<int>> create_example()
+counted_ptr<Node<std::string>> create_example()
 {
-    counted_ptr<Node<int>> n0{new Node{0}};
-    counted_ptr<Node<int>> n1{new Node{1}};
-    counted_ptr<Node<int>> n2{new Node{2}};
-    counted_ptr<Node<int>> n3{new Node{3}};
-    counted_ptr<Node<int>> n4{new Node{4}};
-    counted_ptr<Node<int>> n5{new Node{5}};
+    counted_ptr<Node<std::string>> n0{new Node<std::string>{"0"}};
+    counted_ptr<Node<std::string>> n1{new Node<std::string>{"1"}};
+    counted_ptr<Node<std::string>> n2{new Node<std::string>{"2"}};
+    counted_ptr<Node<std::string>> n3{new Node<std::string>{"3"}};
+    counted_ptr<Node<std::string>> n4{new Node<std::string>{"4"}};
+    counted_ptr<Node<std::string>> n5{new Node<std::string>{"5"}};
 
     n0->insert(n1);
     n0->insert(n2);
@@ -25,9 +26,9 @@ counted_ptr<Node<int>> create_example()
     return n0;
 }
 
-void print_nodes(std::vector<counted_ptr<Node<int>>> nodes)
+void print_nodes(std::vector<counted_ptr<Node<std::string>>> nodes)
 {
-    for (counted_ptr<Node<int>> node : nodes)
+    for (counted_ptr<Node<std::string>> node : nodes)
     {
         std::cout << node->value << " ";
     }
@@ -36,8 +37,8 @@ void print_nodes(std::vector<counted_ptr<Node<int>>> nodes)
 
 int main()
 {
-    counted_ptr<Node<int>> root{create_example()};
-    std::vector<counted_ptr<Node<int>>> nodes{get_all_nodes(root)};
+    counted_ptr<Node<std::string>> root{create_example()};
+    std::vector<counted_ptr<Node<std::string>>> nodes{get_all_nodes(root)};
 
     std::cout << "After insertions: " << std::endl;
     print_nodes(nodes);
